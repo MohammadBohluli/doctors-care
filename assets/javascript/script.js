7b0ec11d94ef6c11d267a68ac9e303c6f384b5ae
MohammadBohluli <mohammad.bohluli8789@gmail.com>
2024-02-12 18:52:46 +0330

    change: wrapper-nav to nav-content

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
-const btnMenu = document.querySelector(".menu-bar");
-const navContent = document.querySelector(".wrapper-nav");
-btnMenu.addEventListener("click", () => {
-  navContent.classList.toggle("hidden");
+const btnMenu = document.querySelector('.menu-bar');
+const navContent = document.querySelector('.nav-content');
+btnMenu.addEventListener('click', () => {
+  navContent.classList.toggle('hidden');
 });
