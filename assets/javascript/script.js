7142ebf9047b57847283df4b1014d1e523f71c95
MohammadBohluli <mohammad.bohluli8789@gmail.com>
2024-02-17 13:30:40 +0330

    feat:revealing hero section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,17 @@
-const btnMenu = document.querySelector('.menu-bar');
-const navContent = document.querySelector('.nav-content');
-const nav = document.querySelector('.nav');
+"use strict";
+const btnMenu = document.querySelector(".menu-bar");
+const navContent = document.querySelector(".nav-content");
+const nav = document.querySelector(".nav");
 const navHeight = nav.getBoundingClientRect().height;
-const hero = document.querySelector('.hero-section');
+const hero = document.querySelector(".hero-section");
+const heroImg = document.querySelector(".hero__img");
+const heroContent = document.querySelector(".hero__content");
 
 /***********************************************************************/
 /********************** Toggle Navbar Menu */
 /***********************************************************************/
-btnMenu.addEventListener('click', () => {
-  navContent.classList.toggle('hidden');
+btnMenu.addEventListener("click", () => {
+  navContent.classList.toggle("hidden");
 });
 
 /***********************************************************************/
@@ -17,17 +20,40 @@ btnMenu.addEventListener('click', () => {
 const stickyNav = function (entries) {
   const [entry] = entries;
 
-  if (!entry.isIntersecting) nav.classList.add('sticky');
+  if (!entry.isIntersecting) nav.classList.add("sticky");
   else {
-    nav.classList.remove('sticky');
+    nav.classList.remove("sticky");
   }
 };
 
-const obsOptions = {
+const navObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
   rootMargin: `${navHeight}px`,
+});
+navObserver.observe(hero);
+
+/***********************************************************************/
+/********************** Revealing Intersection Observer */
+/***********************************************************************/
+const replacing = function (element, oldClass, newClass) {
+  element.classList.replace(oldClass, newClass);
 };
 
-const navObserver = new IntersectionObserver(stickyNav, obsOptions);
-navObserver.observe(hero);
+const revealingSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) {
+    replacing(heroImg, "fade-right", "fade-in");
+    replacing(heroContent, "fade-left", "fade-in");
+  } else {
+    replacing(heroImg, "fade-in", "fade-right");
+    replacing(heroContent, "fade-in", "fade-left");
+  }
+};
+
+const sectionObserver = new IntersectionObserver(revealingSection, {
+  root: null,
+  threshold: 0.01,
+});
+sectionObserver.observe(hero);
